f7e9be52195259d404479e5eda92a0c4d39e537b
hayeon <hy0708@cau.ac.kr>
2023-09-09 16:34:53 +0900

    Initial commit

---
+++ src/main/java/com/example/puddyBuddy/PuddyBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.example.puddyBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PuddyBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PuddyBuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/puddyBuddy/PuddyBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.puddyBuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PuddyBuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
