851c803280a0bef8534050d8151dd6474d3286fe
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-10-29 19:07:53 +0900

    게시글 목록 조회 구현

--- src/main/java/com/example/puddyBuddy/controller/BoardController.java
+++ src/main/java/com/example/puddyBuddy/controller/BoardController.java
@@ -1,15 +1,23 @@
 package com.example.puddyBuddy.controller;
-
+import com.example.puddyBuddy.domain.Board;
 import com.example.puddyBuddy.service.BoardService;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
 
 @Controller
 @RestController
 @RequestMapping( value = "/boards",  produces = "application/json;charset=utf8")
 public class BoardController {
     private final BoardService boardService;
+
+    @GetMapping
+    public @ResponseBody List<Board> getBoards() {
+        List<Board> boards = boardService.getBoards();
+        return boards;
+    }
 
     public BoardController(BoardService boardService) {
         this.boardService = boardService;
