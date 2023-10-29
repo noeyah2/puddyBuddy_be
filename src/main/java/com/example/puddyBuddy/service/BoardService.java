2bd9e41935700d024f6e3021ba3b1513aabd930b
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-10-29 19:07:53 +0900

    게시글 목록 조회 구현

--- src/main/java/com/example/puddyBuddy/service/BoardService.java
+++ src/main/java/com/example/puddyBuddy/service/BoardService.java
@@ -2,20 +2,23 @@ package com.example.puddyBuddy.service;
 
 import com.example.puddyBuddy.domain.Board;
 import com.example.puddyBuddy.repository.BoardRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.io.IOException;
 import java.util.List;
 
 @Service
+//@RequiredArgsConstructor
 public class BoardService {
-    private final BoardRepository userRepository;
+    private final BoardRepository boardRepository;
 
-    public BoardService(BoardRepository userRepository) {
-        this.userRepository = userRepository;
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
     }
 
     public List<Board> getBoards(){
-        List<Board> objs = userRepository.findAll();
-        return objs;
+        List<Board> boards = boardRepository.findAll();
+        return boards;
     }
 }
