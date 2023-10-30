d08826d13fdd7096a8aaa481f20090bd7d644edb
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-10-30 10:39:59 +0900

    remove security file

---
+++ sql/ddl.sql
@@ -0,0 +1,37 @@
+
+INSERT INTO `puddybuddy`.`stores` (`store_id`, `store_name`) VALUES ('1', '앤블랭크');
+INSERT INTO `puddybuddy`.`stores` (`store_id`, `store_name`) VALUES ('2', '예일');
+INSERT INTO `puddybuddy`.`stores` (`store_id`, `store_name`) VALUES ('3', '펫데렐라 프로젝트');
+INSERT INTO `puddybuddy`.`stores` (`store_id`, `store_name`) VALUES ('4', '플로트');
+INSERT INTO `puddybuddy`.`stores` (`store_id`, `store_name`) VALUES ('5', 'MLB');
+
+INSERT INTO `puddybuddy`.`sizes` (`size_id`, `name`) VALUES ('1', 'XS');
+INSERT INTO `puddybuddy`.`sizes` (`size_id`, `name`) VALUES ('2', 'S');
+INSERT INTO `puddybuddy`.`sizes` (`size_id`, `name`) VALUES ('3', 'M');
+INSERT INTO `puddybuddy`.`sizes` (`size_id`, `name`) VALUES ('4', 'L');
+INSERT INTO `puddybuddy`.`sizes` (`size_id`, `name`) VALUES ('5', 'XL');
+
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('1', '블랙');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('2', '그레이');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('3', '화이트');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('4', '블루');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('5', '그린');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('6', '베이지');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('7', '네이비');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('8', '핑크');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('9', '브라운');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('10', '옐로우');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('11', '퍼플');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('12', '레드');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('13', '스카이블루');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('14', '라벤더');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('15', '오렌지');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('16', '민트');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('17', '카키');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('18', '와인');
+INSERT INTO `puddybuddy`.`colors` (`color_id`, `name`) VALUES ('19', '기타');
+
+INSERT INTO `puddybuddy`.`personal_colors` (`personal_color_id`, `name`) VALUES ('1', '봄웜');
+INSERT INTO `puddybuddy`.`personal_colors` (`personal_color_id`, `name`) VALUES ('2', '여름쿨');
+INSERT INTO `puddybuddy`.`personal_colors` (`personal_color_id`, `name`) VALUES ('3', '가을웜');
+INSERT INTO `puddybuddy`.`personal_colors` (`personal_color_id`, `name`) VALUES ('4', '겨울쿨');
---
+++ src/main/java/com/example/puddyBuddy/dto/UploadPhotoDTO.java
@@ -0,0 +1,14 @@
+package com.example.puddyBuddy.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@ToString
+public class UploadPhotoDTO {
+    private String uploadImg;
+}
