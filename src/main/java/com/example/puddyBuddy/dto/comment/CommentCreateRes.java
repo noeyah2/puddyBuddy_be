e26db18fafdd3a84312a96c2037097dba8d30d16
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-01 20:25:58 +0900

    오타

--- src/main/java/com/example/puddyBuddy/dto/comment/CommentCreateRes.java
+++ src/main/java/com/example/puddyBuddy/dto/comment/CommentCreateRes.java
@@ -6,8 +6,8 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Schema(title = "댓글 등록 DTO reponse")
+@Schema(title = "댓글 등록 DTO response")
 public class CommentCreateRes {
     @Schema(description = "댓글 번호", example = "1")
-    private Long commendId;
+    private Long commentId;
 }
