9178a0203dc11821ba6908d8ac8d07a45b92310b
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-06 17:32:02 +0900

    comment response 방식 변경

--- src/main/java/com/example/puddyBuddy/controller/CommentController.java
+++ src/main/java/com/example/puddyBuddy/controller/CommentController.java
@@ -2,10 +2,10 @@ package com.example.puddyBuddy.controller;
 
 import com.example.puddyBuddy.domain.Board;
 import com.example.puddyBuddy.domain.Comment;
-import com.example.puddyBuddy.dto.board.BoardCreateReq;
-import com.example.puddyBuddy.dto.board.BoardCreateRes;
+import com.example.puddyBuddy.domain.User;
 import com.example.puddyBuddy.dto.comment.CommentCreateReq;
 import com.example.puddyBuddy.dto.comment.CommentCreateRes;
+import com.example.puddyBuddy.dto.comment.CommentListRes;
 import com.example.puddyBuddy.exception.common.BusinessException;
 import com.example.puddyBuddy.response.BaseResponse;
 import com.example.puddyBuddy.service.CommentService;
@@ -15,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Controller
 @RestController
@@ -27,11 +28,18 @@ public class CommentController {
         this.commentService = commentService;
     }
 
-    @Operation(summary = "댓글 전부 보기")
+    @Operation(summary = "댓글 전체 목록")
     @GetMapping
-    public @ResponseBody List<Comment> getComments() {
-        List<Comment> Comments = commentService.getComments();
-        return Comments;
+    public BaseResponse<List<CommentListRes>> getUsers() {
+        try {
+            List<Comment> comments = commentService.getComments();
+            List<CommentListRes> commentList = comments.stream()
+                    .map(CommentListRes::new)
+                    .collect(Collectors.toList());
+            return new BaseResponse<>(commentList);
+        } catch(BusinessException e) {
+            return new BaseResponse<>(e.getErrorCode());
+        }
     }
 
     @Operation(summary = "댓글 등록", description = "해당 게시글에 댓글을 등록했습니다.")
