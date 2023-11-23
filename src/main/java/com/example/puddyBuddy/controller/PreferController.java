cf7ee2ec9643d65ec6220f8cee8c6275256cec7a
hayeon <108393035+noeyah2@users.noreply.github.com>
2023-11-23 21:53:26 +0900

    prefer조건번호로 목록 조회 > 복수개의 선호조건 판별 api 구현

--- src/main/java/com/example/puddyBuddy/controller/PreferController.java
+++ src/main/java/com/example/puddyBuddy/controller/PreferController.java
@@ -2,6 +2,7 @@ package com.example.puddyBuddy.controller;
 
 import com.example.puddyBuddy.dto.prefer.PreferCreateReq;
 import com.example.puddyBuddy.dto.prefer.PreferCreateRes;
+import com.example.puddyBuddy.dto.prefer.PreferNumRes;
 import com.example.puddyBuddy.dto.prefer.PreferRes;
 
 import io.swagger.v3.oas.annotations.Operation;
@@ -27,11 +28,22 @@ public class PreferController {
     }
 
     @Operation(summary = "선호조건 회원별로 보기", description = "회원 번호를 넘겨주면 그 회원이 가진 선호 조건 리스트를 보내드립니다.")
-    @GetMapping("/{userId}")
+    @GetMapping("/users/{userId}")
     public BaseResponse<List<PreferRes>> getPreferUser(@PathVariable Long userId){
         try {
             List<PreferRes> preferResList = preferService.getPreferUser(userId);
             return new BaseResponse<>(preferResList);
+        } catch (BusinessException e) {
+            return new BaseResponse<>(e.getErrorCode());
+        }
+    }
+
+    @Operation(summary = "선호조건 아이디로 검색", description = "선호조건 번호를 넘겨주면 해당 선호 조건 리스트를 보내드립니다.")
+    @GetMapping("/{preferId}")
+    public BaseResponse<List<PreferNumRes>> getPreferId(@PathVariable Long preferId){
+        try {
+            List<PreferNumRes> prefer = preferService.getPreferId(preferId);
+            return new BaseResponse<>(prefer);
         } catch (BusinessException e) {
             return new BaseResponse<>(e.getErrorCode());
         }
