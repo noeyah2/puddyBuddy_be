74de0bcc6c61b54e0254aab5e7ea8abdb0523e85
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-11-27 03:00:46 +0900

    11.27 커밋

--- src/main/java/com/example/puddyBuddy/service/PetsnalColorService.java
+++ src/main/java/com/example/puddyBuddy/service/PetsnalColorService.java
@@ -5,6 +5,7 @@ import com.example.puddyBuddy.dto.PetsnalColor.PetsnalColorStartRes;
 import com.example.puddyBuddy.repository.PetsnalColorRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -26,11 +27,49 @@ public class PetsnalColorService {
 
     public PetsnalColorRes doTest(int stage,Long PreferId, List<Integer> resultList){
         // 결과 분석
-
-
+        // next stage, photo_url_list, result 세팅
         PetsnalColorRes res = new PetsnalColorRes();
-        // nest stage, photo_url_list, result 세팅
-        // nest stage, photo_url_list, result 세팅
+        int nextStage = 0;
+        int result = 0;
+        List<String> list = new ArrayList<>();
+        switch (stage){
+            case 1:
+                nextStage = testResult(2, 3, resultList);
+                res.setNextStage(nextStage);
+                break;
+            case 2:
+                break;
+            case 3:
+                res.setNextStage(nextStage);
+                res.setPhotoUrlList(list);
+                res.setResult(testResult(4, 2, resultList));
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            default:
+                break;
+        }
+
+
         return res;
+    }
+
+    private int testResult(int a, int b, List<Integer> resultList){
+        if (resultList.size() >6){
+            return (resultList.get(6) == 0? 2 : 3); // 0은 warm, 1은 cool
+        }else {
+            int evenSum = 0;
+            int oddSum = 0;
+            for (int i = 0; i < resultList.size(); i++) {
+                if (i % 2 == 0) {
+                    evenSum += resultList.get(i);
+                } else {
+                    oddSum += resultList.get(i);
+                }
+            }
+            return (evenSum>oddSum? 2 :3);
+        }
     }
 }
