970b681554d67c2334e43a1f094a327ce000fb64
Ssun2zang <98401161+Ssun2zang@users.noreply.github.com>
2023-11-27 01:46:34 +0900

    petsnal color 컨트롤러 api 추가

--- src/main/java/com/example/puddyBuddy/service/PetsnalColorService.java
+++ src/main/java/com/example/puddyBuddy/service/PetsnalColorService.java
@@ -1,5 +1,6 @@
 package com.example.puddyBuddy.service;
 
+import com.example.puddyBuddy.dto.PetsnalColor.PetsnalColorStartRes;
 import com.example.puddyBuddy.repository.PetsnalColorRepository;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,14 @@ public class PetsnalColorService {
 
     public PetsnalColorService(PetsnalColorRepository petsnalColorRepository) {
         this.petsnalColorRepository = petsnalColorRepository;
+    }
+
+    public PetsnalColorStartRes makePetsnalTest(Long PreferId, String img){
+        // flask 서버로 요청
+        // => preferid-img petsnalcolors db에 저장
+
+        PetsnalColorStartRes res = new PetsnalColorStartRes();
+        res.setResult(true);
+        return res;
     }
 }
