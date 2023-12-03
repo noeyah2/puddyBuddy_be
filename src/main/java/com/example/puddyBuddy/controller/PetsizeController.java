121c701592f1983a074d4808091ff600c3fe7fe0
noeyah2 <hy0708@cau.ac.kr>
2023-12-03 20:13:32 +0900

    DB error 수정

---
+++ src/main/java/com/example/puddyBuddy/controller/PetsizeController.java
@@ -0,0 +1,52 @@
+package com.example.puddyBuddy.controller;
+
+import com.example.puddyBuddy.dto.petsize.PetsizeCreateReq;
+import com.example.puddyBuddy.dto.petsize.PetsizeCreateRes;
+import com.example.puddyBuddy.dto.petsize.PetsizeInfoReq;
+import com.example.puddyBuddy.dto.petsize.PetsizeInfoRes;
+import com.example.puddyBuddy.dto.prefer.PreferCreateRes;
+import com.example.puddyBuddy.exception.common.BusinessException;
+import com.example.puddyBuddy.response.BaseResponse;
+import com.example.puddyBuddy.service.PetsizeService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+
+@Controller
+@RestController
+@RequestMapping( value = "/petsize",  produces = "application/json;charset=utf8")
+@Tag(name = "펫사이즈 API")
+public class PetsizeController {
+    private final PetsizeService petsizeService;
+
+    public PetsizeController(PetsizeService petsizeService) {
+        this.petsizeService = petsizeService;
+    }
+
+    @Operation(summary = "사이즈 등록", description = "사이즈 정보를 주면 그걸 등록합니다.")
+    @PostMapping("/create")
+    public BaseResponse<PetsizeCreateRes> createSize(@RequestBody PetsizeCreateReq petsizeCreateReq){
+        try {
+            PetsizeCreateRes petsizeCreateRes = petsizeService.createSize(petsizeCreateReq.getPreferId(), petsizeCreateReq.getNeck(), petsizeCreateReq.getChest(), petsizeCreateReq.getBack(), petsizeCreateReq.getLeg());
+            return new BaseResponse<>(petsizeCreateRes);
+        } catch (BusinessException e) {
+            return new BaseResponse<>(e.getErrorCode());
+        }
+    }
+
+    @Operation(summary = "펫 사이즈 정보 조회", description = "펫 사이즈 정보를 조회합니다.")
+    @PostMapping("/getPercentages")
+    public BaseResponse<PetsizeInfoRes> getPercentages(@RequestBody PetsizeInfoReq petsizeInfoReq) {
+        try {
+            PetsizeInfoRes petsizeInfoRes = petsizeService.getPercentages(
+                    petsizeInfoReq.getBreedTag().getBreedTagId(),
+                    petsizeInfoReq.getPetsizeId()
+            );
+            return new BaseResponse<>(petsizeInfoRes);
+        } catch (BusinessException e) {
+            return new BaseResponse<>(e.getErrorCode());
+        }
+    }
+}
